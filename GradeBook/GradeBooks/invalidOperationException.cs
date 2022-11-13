f6925ef0f24927e2786c45f4023c646464240c5a
Mohit4mr <mohit4mr@gmail.com>
2022-11-13 13:07:50 +0530

    GradeBooks

---
+++ GradeBook/GradeBooks/invalidOperationException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace GradeBook.GradeBooks
+{
+    [Serializable]
+    internal class invalidOperationException : Exception
+    {
+        public invalidOperationException()
+        {
+        }
+
+        public invalidOperationException(string message) : base(message)
+        {
+        }
+
+        public invalidOperationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected invalidOperationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
